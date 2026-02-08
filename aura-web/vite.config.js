2e2c0cf32181626757c683d73ed7f961a0ed73b1
Mohit20198 <101410376+Mohit20198@users.noreply.github.com>
2026-02-08 11:51:30 +0530

    Update vite.config.js

--- aura-web/vite.config.js
+++ aura-web/vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/aura-web/', // This MUST match your repository name exactly
   plugins: [react()],
 })
